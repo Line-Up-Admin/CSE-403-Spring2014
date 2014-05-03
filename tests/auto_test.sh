db6eece7bc2e3b7526faebc819db9e129185b13c
root <root@ip-172-31-13-61.us-west-2.compute.internal>
2014-05-03 19:46:42 +0000

    tests:

--- tests/auto_test.sh
+++ tests/auto_test.sh
@@ -1,14 +1,25 @@
 #!/bash/bin
 
-
+#clean up
+cd /var/www
+rm -rf lineup_test
 
 #copy email template
-cp testlogs/test_output_template.txt testlogs/test_output.txt
+#cp testlogs/test_output_template.txt testlogs/test_output.txt
+git clone https://github.com/Line-Up-Admin/CSE-403-Spring2014.git /var/www/lineup_test
+
+
+cd /var/www/lineup_test/tests
 
 #run python tests and redirect output to file
-python flask_tests/test.py 2>> testlogs/test_output.txt
+python flask_tests/test.py 2> test_output.txt
+
+#run angular tests and redirect output to file
+phantomjs angular_tests/auto_jasmine_tests.js 1>> test_output.txt
 
 
 #email results 
-ssmtp laplansk@gmail.com < testlogs/test_output.txt
-#ssntp cse403group@uw.edu < test_output.txt
+ssmtp cse403group@uw.edu < test_output.txt
+
+
+
