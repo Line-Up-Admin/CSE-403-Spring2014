b135db7f70023c8faa6e2e0f47328882b1c96831
Nicholas Shahan <nick.shahan@gmail.com>
2014-05-02 21:29:23 -0700

    auto test parse error

--- tests/angular_tests/auto_jasmine_tests.js
+++ tests/angular_tests/auto_jasmine_tests.js
@@ -6,12 +6,12 @@ page.open('file:///var/www/lineup/tests/angular_tests/SpecRunner.html', function
   });
   console.log('Page title is ' + title);
 
-  results = page.evaluate(function() {
-    element = document.getElementsByClassName('bar');
-    return element[0].textContent;
-    }
-  }
+  // results = page.evaluate(function() {
+  //   element = document.getElementsByClassName('bar');
+  //   return element[0].textContent;
+  //   }
+  // }
 
-  console.log("Angular Tests: " + results);
+  // console.log("Angular Tests: " + results);
   phantom.exit();
 });
