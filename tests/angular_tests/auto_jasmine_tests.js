3a40d6e02a3293e172e90909bc4e793e4bdc9331
Nicholas Shahan <nick.shahan@gmail.com>
2014-05-02 21:10:44 -0700

    fixed file name mismatch

--- tests/angular_tests/auto_jasmine_tests.js
+++ tests/angular_tests/auto_jasmine_tests.js
@@ -1,11 +1,10 @@
 var page = require('webpage').create();
-page.open('/var/www/lineup/tests/angular_tests/SpecRunner.html', function(status) {
+page.open('file:///var/www/lineup/tests/angular_tests/SpecRunner.html', function(status) {
 
   var title = page.evaluate(function() {
     return document.title;
   });
   console.log('Page title is ' + title);
-
 
   results = page.evaluate(function() {
     element = $('.bar.failed').text();
