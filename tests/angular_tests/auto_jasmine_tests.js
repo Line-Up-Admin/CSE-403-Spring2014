cf12ebb22021e9b980eae14fa4174c8b179f9083
Nicholas Shahan <nick.shahan@gmail.com>
2014-05-02 21:32:28 -0700

    fixed error in auto tests

--- tests/angular_tests/auto_jasmine_tests.js
+++ tests/angular_tests/auto_jasmine_tests.js
@@ -1,17 +1,17 @@
 var page = require('webpage').create();
 page.open('file:///var/www/lineup/tests/angular_tests/SpecRunner.html', function(status) {
 
-  var title = page.evaluate(function() {
-    return document.title;
+  // var title = page.evaluate(function() {
+  //   return document.title;
+  // });
+  // console.log('Page title is ' + title);
+
+  results = page.evaluate(function() {
+    element = document.getElementsByClassName('bar');
+    return element[0].textContent;
+    }
   });
-  console.log('Page title is ' + title);
 
-  // results = page.evaluate(function() {
-  //   element = document.getElementsByClassName('bar');
-  //   return element[0].textContent;
-  //   }
-  // }
-
-  // console.log("Angular Tests: " + results);
+  console.log("Angular Tests: " + results);
   phantom.exit();
 });
