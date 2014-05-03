db49bc7afcbb4cf8e36aebdbea3d32b0a10bfa07
Nicholas Shahan <nick.shahan@gmail.com>
2014-05-02 20:59:51 -0700

    new auto test file again

--- tests/angular_tests/auto_jasmine_tests.js
+++ tests/angular_tests/auto_jasmine_tests.js
@@ -1,4 +1,20 @@
 var page = require('webpage').create();
-page.open('SpecRunner.html', function() {
+page.open('/var/www/lineup/tests/angular_tests/SpecRunner.html', function(status) {
+
+  var title = page.evaluate(function() {
+    return document.title;
+  });
+  console.log('Page title is ' + title);
+
+
+  results = page.evaluate(function() {
+    element = $('.bar.failed').text();
+    if (element == undefined) {
+      return "All Passed";
+    } else {
+      return element.text();
+    }
+    console.log("Angular Tests: " + results);
+
   phantom.exit();
 });
