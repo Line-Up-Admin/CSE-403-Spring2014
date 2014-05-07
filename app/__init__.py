2c0b68851a4f5f7b90c5a0ef61e4386eae94c135
ThomasRothschilds <tgr4@uw.edu>
2014-05-07 14:13:18 -0700

    updated database utilities. __init__ now creates a database of no database existed.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,46 @@
 # It also routes default paths to the /app/static folder
 # The route handlers are imported from views.
 
-from flask import Flask
-
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+  render_template, flash
 app = Flask(__name__, static_url_path='')
+
+database_file = os.path.join(app.root_path, 'app.db')
+schema_file = os.path.join(app.root_path, 'schema.sql')
+
+def connect_db():
+  """Connects to the specific database."""
+  rv = sqlite3.connect(database_file)
+  rv.row_factory = sqlite3.Row
+  return rv
+
+def get_db():
+  """Opens a new database connection if there is none yet for the
+  current application context.
+  """
+  with app.app_context():
+    if not hasattr(g, 'sqlite_db'):
+      g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def init_db():
+  """Initializes the database."""
+  with app.app_context():
+    db = get_db()
+    with app.open_resource(schema_file, mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+
+@app.teardown_appcontext
+def close_db(error):
+  """Closes the database again at the end of the request."""
+  if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close()
+
+# only init the db if it doesn't exist.
+if not os.path.isfile(database_file):
+  init_db()
+
 from app import views
--- app/database_utilities.py
+++ app/database_utilities.py
@@ -9,8 +9,20 @@ __init__.py in app. All methods are static.
 
 
 # Database Utilities
+import permissions
+
 class DatabaseException(Exception):
   pass
+
+class PermissionException(Exception):
+  pass
+
+class ValidationException(Exception):
+  pass
+
+#############################################
+# User related utilities.
+#############################################
 
 def create_user(user_data):
   """Adds a user defined by user_data to the database.
@@ -23,39 +35,70 @@ def create_user(user_data):
 
   Raises:
     DatabaseException: the database operation failed.
+    ValueError: the given user_data is invalid.
   """
   raise NotImplementedError()
 
 def modify_user(user_data):
-  """Modfiy the user defined by the id in user_data to match the user_data.
+  """Modfiy the user to match the user_data.
+
+  The uid, username, and password are obtained from the current session, not the user_data.
 
   Args:
     user_data: the data about the user to be modified in the database. The id will be used to find the user in the database.
 
   Returns:
-    void if the user's data was successfully updated.
+    None if the user's data was successfully updated.
 
   Raises:
-    DatabaseException: the database operation failed.
+    DatabaseException: the uid does not exist in the database.
+    ValidationException: the current session user is not logged in.
+    ValueError: the given user_data is invalid.
   """
   raise NotImplementedError()
 
-def delete_user(user_data):
-  """Deletes a user from the database.
-
-  Args:
-    user_data: the user to be deleted. The username and password must match.
+def delete_user():
+  """Deletes the current session user from the database.
 
   Returns:
-    void if the deletion was a success.
+    None if the deletion was a success.
 
   Raises:
-    DatabaseException: the database operation failed.
+    DatabaseException: the uid does not exist in the database.
+    ValidationException: the current session user is not logged in.
+    PermissionException: the current session does not have the required permissions.
   """
   raise NotImplementedError()
 
+def get_user():
+  """Retrieves the user data associated with the current session user.
+
+  Returns:
+    A User object if the user was found. No temporary users are considered.
+
+  Raises:
+    DatabaseException: the user data was not found.
+    ValidationException: the current session user is not logged in.
+  """
+  raise NotImplementedError()
+
+def get_temp_user(temp_uid):
+  """Retrieves the user data associated with the given user id, only if the uid matches a temporary user.
+
+  Returns:
+    A User object if the user was found. Only temporary users are examined.
+
+  Raises:
+    DatabaseException: the temp_uid was not a temporary user.
+  """
+  raise NotImplementedError()
+
+#################################################
+# Queue related utilities.
+#################################################
+
 def create_queue(q_settings):
-  """Creates a new queue with the defined settings. All settings that are left blank will be left as Null.
+  """Creates a new queue with the defined settings. All settings except qid must exist.
 
   Args:
     q_settings: the settings for the new queue. The qid will be ignored if included.
@@ -64,12 +107,14 @@ def create_queue(q_settings):
     The new qid if the queue was successfully created.
 
   Raises:
-    DatabaseException: the database operation failed.
+    DatabaseException: the q_settings are invalid.
   """
   raise NotImplementedError()
 
-def modify_queue(q_settings):
+def modify_queue_settings(q_settings):
   """Modifies the queue with the qid defined in q_settings to match the q_settings given.
+
+  If the user's session does not have proper permissions, a PermissionException will be raised.
 
   Args:
     q_settings: the settings for the queue to be modified. The qid will be used to find the queue in the database.
@@ -78,8 +123,11 @@ def modify_queue(q_settings):
     void if the queue's settings were successfully updated.
 
   Raises:
-    DatabaseException: the database operation failed.
-  """
+    DatabaseException:
+      (1) The queue doesn't exist.
+      (2) The q_settings are invalid.
+    PermissionException: the current session user does not have permission to modify this queue's settings.
+  """ 
   raise NotImplementedError()
 
 def delete_queue(qid):
@@ -92,6 +140,18 @@ def delete_queue(qid):
     void if the queue was successfully deleted.
 
   Raises:
-    DatabaseException: the database operation failed.
+    DatabaseException: the queue doesn't exist.
+    PermissionException: the current session user does not have permission to delete this queue.
   """
   raise NotImplementedError()
+
+def get_queue_settings(qid):
+  """Retrieves the queue_settings associated with qid.
+
+  Returns:
+    The QSettings associated with the qid.
+
+  Raises:
+    DatabaseException: the queue doesn't exist.
+    PermissionException: the current session user does not have permission to view this queue.
+  """
