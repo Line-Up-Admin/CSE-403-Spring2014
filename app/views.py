ef73b1a317cbbd98c4bde5f0f9565cb20c435b53
Thomas Rothschilds <tgr4@uw.edu>
2014-05-09 00:07:36 -0700

    fix to views so that app can run.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 # This is the file that contains all the route handlers.
 from app import app
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import database_utilities as db_util
+import user_module
+from flask import request, session, g, redirect, url_for, abort, jsonify
+
 from q_classes import QueueServer
 from user_module import UserModule
-app = Flask(__name__)
-app.config.from_object(__name__)
 server = QueueServer()
 module = UserModule()
 
@@ -13,59 +14,86 @@ module = UserModule()
 def root():
     return app.send_static_file('index.html')
 
-@app.route('/join', methods=['POST'])
+@app.route('/join')
 def add_to_queue():
-    userID = request.form['userID']
-    queueID = request.form['queueID']
-    queue_result = server.add(userID, queueID)
-    #return JSON version of queue
+    return 'Not implemented yet!'
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        result = module.login(username, password)
-        # EVAN: is result the userID?
-    else:
-        # THOMAS: so I shouldn't redirect to static login page? 
-        #what if method is GET?
-        pass
+    return 'Not implemented yet!'
 
-@app.route('/dequeue', methods=['POST'])
+@app.route('/dequeue')
 def dequeue():
-    queueID = request.form['queueID']
-    employeeID = request.form['userID']
-    member = server.dequeue(employeeID, queueID)
-    # return JSON QueueMember
-
+    return 'Not implemented yet!'
+	
 @app.route('/searchResults')
+def get_search_results():
+	return 'Not implemented yet!'
 
 @app.route('/search')
+def search():
+	return 'Not implemented yet!'
 
-@app.route('/memberQueue')
+@app.route('/memberQueue/<qid>')
+def get_member_queue(qid):
+	return 'Not implemented yet!'
 
-@app.route('/employeeQueue')
+@app.route('/employeeQueue/<qid>')
+def get_employee_queue(qid):
+	return 'Not implemented yet!'
 
-@app.route('/adminQueue')
+@app.route('/adminQueue/<qid>')
+def get_admin_queue(qid):
+	return 
 
 @app.route('/queueStatus')
+def get_queue_status():
+	return 'Not implemented yet!'
 
 @app.route('/myQueues')
+def get_my_queues():
+	return 'Not implemented yet!'
 
 @app.route('/remove')
+def remove_queue_member():
+	return 'Not implemented yet!'
 
 @app.route('/qtracks')
+def queue_tracks():
+	return 'Not implemented yet!'
 
 @app.route('/qtracksData')
+def queue_tracks_data():
+	return 'Not implemented yet!'
 
 @app.route('/createUser')
+def create_user():
+	return 'Not implemented yet!'
 
 @app.route('/createQueue')
+def create_queue():
+	return 'Not implemented yet!'
 
 @app.route('/logout')
+def logout():
+	return 'Not implemented yet!'
 
 # Takes the '/helloworld' route and returns "Hello, World!"
 @app.route('/helloworld')
-def index():
+def hello_world():
     return "Hello, World!"
+
+# Temporary: debugging purposes only.
+@app.route('/user')
+def get_user():
+	username = request.args.get('username')
+	password = request.args.get('password')
+	try:
+		user = db_util.get_user(username, password)
+		return jsonify(user)
+	except db_util.DatabaseException as e:
+		return e.message
+	except db_util.ValidationException as e:
+		return e.message
+	
+		
