62f0f800e8c38ecc73dd3594bc016e895b3c2822
Stephen <laplansk@gmail.com>
2014-05-13 21:57:14 -0700

    changed testing script

--- tests/auto_test.sh
+++ tests/auto_test.sh
@@ -10,10 +10,10 @@ git clone https://github.com/Line-Up-Admin/CSE-403-Spring2014.git ~/LineUpTestSp
 cd ~/LineUpTestSpace/lineUp/tests/
 
 # run python tests and redirect output to file
-python flask_tests/test.py 2> test_output.txt
+python ~/LineUpTestSpace/lineUp/tests/flask_tests/test.py 2> test_output.txt
 
 # run angular tests and redirect output to file
-phantomjs angular_tests/auto_jasmine_tests.js 1>> test_output.txt
+phantomjs ~/LineUpTestSpace/lineUp/tests/angular_tests/auto_jasmine_tests.js 1>> test_output.txt
 
 # email results 
 ssmtp cse403group@uw.edu < test_output.txt
