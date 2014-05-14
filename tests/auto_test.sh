42818a20542781a2b859fb6b670b6c2f5000cd81
Stephen <laplansk@gmail.com>
2014-05-13 19:45:12 -0700

    added/changed testing scripts

--- tests/auto_test.sh
+++ tests/auto_test.sh
@@ -1,25 +1,20 @@
 #!/bash/bin
 
-#clean up
-cd /var/www
-rm -rf lineup_test
+# clean up any repos from previous tests
+rm -rf ~/LineUpTestSpace/CSE-403-Spring2014
 
 #copy email template
 #cp testlogs/test_output_template.txt testlogs/test_output.txt
-git clone https://github.com/Line-Up-Admin/CSE-403-Spring2014.git /var/www/lineup_test
+git clone https://github.com/Line-Up-Admin/CSE-403-Spring2014.git ~/LineUpTestSpace
 
 
-cd /var/www/lineup_test/tests
+cd ~/LineUpTestSpace/CSE-403-Spring2014/tests/
 
-#run python tests and redirect output to file
+# run python tests and redirect output to file
 python flask_tests/test.py 2> test_output.txt
 
-#run angular tests and redirect output to file
+# run angular tests and redirect output to file
 phantomjs angular_tests/auto_jasmine_tests.js 1>> test_output.txt
 
-
-#email results 
+# email results 
 ssmtp cse403group@uw.edu < test_output.txt
-
-
-
---
+++ tests/auto_test_setup.sh
@@ -0,0 +1,13 @@
+#!/bash/bin
+
+
+# navigate to home folder and create a scratchspace dir for testing
+cd ~
+mkdir LineUpTestSpace
+
+# install PhantomJS
+sudo apt-get install fontconfig
+sudo apt-get install phantomjs
+
+# install ssmtp
+sudo apt-get install ssmpt
