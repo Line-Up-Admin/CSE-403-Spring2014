631344e6c87eccd8f1ec776c935a2e80202b1cba
ThomasRothschilds <tgr4@uw.edu>
2014-05-23 19:38:03 -0700

    sync_db in postpone

--- app/q_classes.py
+++ app/q_classes.py
@@ -93,8 +93,7 @@ class Queue(object):
       del q[pos]
       return True
 
-   def postpone(self, member):
-      #TODO: fix postpone to sync with db
+   def postpone(self, member, sync_db = False):
       """ Postpones a Queue Member's position in a Queue.
          If a user attempt to postpone past the end of the line,
          the position is not affected."""
@@ -105,8 +104,8 @@ class Queue(object):
          #There is room to move the user back a position in the queue.
          temp = self.my_q[pos]
          next_member = self.my_q[pos + 1]
-        # if sync_db:
-        #    db_util.swap(temp[0].uid, next_member[0].uid, self.id)
+         if sync_db:
+            db_util.swap(temp[0].uid, next_member[0].uid, self.id)
          self.my_q[pos] = self.my_q[pos + 1]
          self.my_q[pos + 1] = temp
       #else: member is already at the end of the queue
