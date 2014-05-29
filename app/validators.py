ea4b1c1a653be76331749e948be4b22ec37f061b
ThomasRothschilds <tgr4@uw.edu>
2014-05-28 20:56:28 -0700

    fix to validators. Now returns only the fields with errors if there was an error.

--- app/validators.py
+++ app/validators.py
@@ -22,10 +22,10 @@ USER_MAX_STR_LEN = {
    'pw': 32
 }
 
-def check_max_str_len(key, dictionary, max_dict):
+def check_max_str_len(key, dictionary, max_dict, fail):
    if dictionary.has_key(key):
       if len(dictionary[key]) > max_dict[key]:
-         dictionary[key] = key + ' text is too long. Max length is ' + max_dict[key]
+         fail[key] = key + ' text is too long. Max length is ' + max_dict[key]
          dictionary['SUCCESS'] = False
    
 def are_matching(encrypted_password, salt, given_password):
@@ -50,35 +50,37 @@ def get_unique_queue_id():
 
 def validate_q_settings(q_settings):
    q_settings['SUCCESS'] = True
+   fail = dict()
+   fail['SUCCESS'] = False
    if not q_settings.has_key('qname') or q_settings['qname'] is None or len(q_settings['qname']) == 0:
-      q_settings['qname'] = 'Required'
+      fail['qname'] = 'Required'
       q_settings['SUCCESS'] = False
    else:
-      check_max_str_len('qname', q_settings, Q_MAX_STR_LEN)
-   check_max_str_len('keywords', q_settings, Q_MAX_STR_LEN)
-   check_max_str_len('location', q_settings, Q_MAX_STR_LEN)  
-   check_max_str_len('website', q_settings, Q_MAX_STR_LEN)
-   check_max_str_len('organization', q_settings, Q_MAX_STR_LEN)
-   check_max_str_len('disclaimer', q_settings, Q_MAX_STR_LEN)
-   check_max_str_len('prompt', q_settings, Q_MAX_STR_LEN)
+      check_max_str_len('qname', q_settings, Q_MAX_STR_LEN, fail)
+   check_max_str_len('keywords', q_settings, Q_MAX_STR_LEN, fail)
+   check_max_str_len('location', q_settings, Q_MAX_STR_LEN, fail)  
+   check_max_str_len('website', q_settings, Q_MAX_STR_LEN, fail)
+   check_max_str_len('organization', q_settings, Q_MAX_STR_LEN, fail)
+   check_max_str_len('disclaimer', q_settings, Q_MAX_STR_LEN, fail)
+   check_max_str_len('prompt', q_settings, Q_MAX_STR_LEN, fail)
    if q_settings.has_key('max_size'):
       try:
          max_size = int(q_settings['max_size'])
       except ValueError:
-         q_settings['max_size'] = 'Invalid maximum size.'
+         fail['max_size'] = 'Invalid maximum size.'
          q_settings['SUCCESS'] = False
       if max_size < 1:
-         q_settings['max_size'] = 'Max size must be greater than zero.'
+         fail['max_size'] = 'Max size must be greater than zero.'
          q_settings['SUCCESS'] = False
    if q_settings.has_key('min_wait_rejoin'):
       try:
          mwr = int(q_settings['min_wait_rejoin'])
       except ValueError:
-         q_settings['min_wait_rejoin'] = 'Invalid minimum wait to rejoin queue.'
-         failure = True
+         fail['min_wait_rejoin'] = 'Invalid minimum wait to rejoin queue.'
+         q_settings['SUCCESS'] = False
       if mwr < 0:
-         q_settings['min_wait_rejoin'] = 'Minimum wait to rejoin queue must be non-negative.'
-         failure = True
+         fail['min_wait_rejoin'] = 'Minimum wait to rejoin queue must be non-negative.'
+         q_settings['SUCCESS'] = False
    if not q_settings.has_key('admins'):
       q_settings['admins']= list()
    else:
@@ -89,6 +91,8 @@ def validate_q_settings(q_settings):
       q_settings['managers'] = list(set(e.strip() for e in q_settings['managers'].split(',')))
    if q_settings.has_key('blocked_users'):
       q_settings['blocked_users'] = list(set(b.strip() for b in q_settings['blocked_users'].split(',')))
+   if not q_settings['SUCCESS']:
+      return fail
    return q_settings
 
 def validate_user(user):
@@ -102,28 +106,32 @@ def validate_user(user):
 	pw varchar(32),
    """
    user['SUCCESS'] = True
+   fail = dict()
+   fail['SUCCESS'] = False
    if not user.has_key('uname') or user['uname'] is None or len(user['uname']) == 0:
-      user['uname'] = 'Required'
+      fail['uname'] = 'Required'
       user['SUCCESS'] = False
    elif ',' in user['uname']:
-      user['uname'] = 'commas are not allowed in the username.'
+      fail['uname'] = 'commas are not allowed in the username.'
       user['SUCCESS'] = False
    else:
-      check_max_str_len('uname', user, USER_MAX_STR_LEN)
-   check_max_str_len('fname', user, USER_MAX_STR_LEN)
-   check_max_str_len('lname', user, USER_MAX_STR_LEN)
-   check_max_str_len('email', user, USER_MAX_STR_LEN)
+      check_max_str_len('uname', user, USER_MAX_STR_LEN, fail)
+   check_max_str_len('fname', user, USER_MAX_STR_LEN, fail)
+   check_max_str_len('lname', user, USER_MAX_STR_LEN, fail)
+   check_max_str_len('email', user, USER_MAX_STR_LEN, fail)
    """The next line should be removed once the proper regex is found."""
-   check_max_str_len('pw', user, USER_MAX_STR_LEN)
+   check_max_str_len('pw', user, USER_MAX_STR_LEN, fail)
    """
    if user.has_key('pw'):
       if not re.match('NEED REGEX HERE', user['pw']):
          user['pw'] = 'password does not meet complexity requirements.'
          user['SUCCESS'] = False
       else:
-         check_max_str_len('pw', user, USER_MAX_STR_LEN)
+         check_max_str_len('pw', user, USER_MAX_STR_LEN, fail)
    else:
       user['pw'] = 'Required.'
       user['SUCCESS'] = False
    """
+   if not user['SUCCESS']:
+      return fail
    return user
