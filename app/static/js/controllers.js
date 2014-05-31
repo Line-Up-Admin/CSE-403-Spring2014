efda6526344e71696c58b9e34abf85a4e37c3662
dju90 <dju90@uw.edu>
2014-05-31 03:42:33 -0700

    Users can't attempt to join queue when closed
    RoundMultipleTimes for user home

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -183,9 +183,9 @@ angular.module('LineUpApp.controllers', []).
       lineUpAPIService.getUsersQueues().
         success(function (data, status, headers, config) {
           if (data.SUCCESS) {
-            roundTimes(data.queues_in);
-            roundTimes(data.queues_admin);
-            roundTimes(data.queues_manager);
+            roundMultipleTimes(data.queues_in);
+            roundMultipleTimes(data.queues_admin);
+            roundMultipleTimes(data.queues_manager);
             if (data.queues_in.length == 0) {
               document.getElementById('empty-in').classList.remove('hide');
             }
@@ -283,9 +283,7 @@ angular.module('LineUpApp.controllers', []).
           roundTimes(data);
           $scope.queue = data;
 					var location = data.location.split(" ").join("%20");
-					console.log(location);
 					$scope.locationLink = "http://maps.google.com/?q=" + location;
-					console.log($scope.locationLink);
           // hide all elements
           document.getElementById('enqueued').classList.add('hide');
           document.getElementById('notEnqueued').classList.add('hide');
@@ -296,11 +294,17 @@ angular.module('LineUpApp.controllers', []).
           } else {
             // show elements for the user that is in the queue
             document.getElementById('enqueued').classList.remove('hide');
+						var bar = document.getElementById("progress");
+						var width = 100.0 / $scope.queue.size;
+						var widthPercentage = width + "%";
+						$scope.progressBar();
           }
-					var bar = document.getElementById("progress");
-					var width = 100.0 / $scope.queue.size;
-					var widthPercentage = width + "%";
-					$scope.progressBar();
+					
+					if (data.active == 0 ) {
+						document.getElementById("btn-join").disabled = true;
+						document.getElementById("closed-message").classList.remove('hide');
+					}
+					
 				}).
         error(function (data, status, headers, config) {
           // not an error we are prepared to handle
@@ -472,6 +476,7 @@ angular.module('LineUpApp.controllers', []).
 		$scope.member_list = [];
 		$scope.setActiveStatusTo = "Close Queue";
     $scope.qid = $routeParams.qid;
+		$scope.close_icon = "glyphicon glyphicon-stop";
 
 		// Redirects to edit queue page.
 		$scope.redirectToEditQueue = function () {
@@ -506,9 +511,11 @@ angular.module('LineUpApp.controllers', []).
   					if( $scope.queueInfo.active == 0 ) {
   						$scope.setActiveStatusTo = "Open Queue";
   						button.value = 1;
+							$scope.close_icon = "glyphicon glyphicon-play";
   						$scope.activeStatus = "CLOSED";
   					} else {
   						$scope.setActiveStatusTo = "Close Queue";
+							$scope.close_icon = "glyphicon glyphicon-stop";
   						button.value = 0;
   					}
   					// gets the selected user
@@ -599,6 +606,12 @@ angular.module('LineUpApp.controllers', []).
       if (!$scope.user.optional_data) {
         $scope.user.optional_data = "";
       }
+			console.log($scope.activeStatus);
+			if( $scope.activeStatus == "CLOSED" ) {
+				$scope.errors = {'error_message': "You cannot add people; queue is inactive."};
+				console.log($scope.errors);
+				document.getElementById('error').classList.remove('hide');
+			}
 
       // send the request
 			lineUpAPIService.enqueue($routeParams.qid, $scope.user).
@@ -641,13 +654,13 @@ angular.module('LineUpApp.controllers', []).
     // sends a request to the server to move the user back one position
 		$scope.demoteSelectPerson = function () {
 			var selectIndex = document.getElementById("list-group").options.selectedIndex;
-			console.log("selected index on trigger is " + selectIndex);
+			//console.log("selected index on trigger is " + selectIndex);
 			if( selectIndex != $scope.member_list.length - 1 ) {
 				lineUpAPIService.demoteSelectPerson({ 'qid': $routeParams.qid, 'uid': $scope.selectedUser.uid }).
 					success(function (data, status, headers, config) {
 						// refresh the queue
 						$scope.getDetailedQueueInfo();
-/* 						console.log($scope.member_list);
+/* 					console.log($scope.member_list);
 						console.log(document.getElementById("list-group").options);	document.getElementById("list-group").options[selectIndex+1].selected="selected";
 						//$scope.selectedUser = $scope.member_list[selectIndex];
 						//WHY DOESN'T THIS WORK!??!?!?! (attempting to keep the selected index)
@@ -678,11 +691,13 @@ angular.module('LineUpApp.controllers', []).
             // the queue is closed
 						$scope.setActiveStatusTo = "Open Queue";
 						button.value = 1;
+						$scope.close_icon = "glyphicon glyphicon-play";
 						$scope.activeStatus = "CLOSED";
 					} else {
             // the queue is open
 						$scope.setActiveStatusTo = "Close Queue";
 						button.value = 0;
+						$scope.close_icon = "glyphicon glyphicon-stop";
 						$scope.activeStatus = "OPEN";
 					}
 				}).
